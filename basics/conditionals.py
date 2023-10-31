85a6f0593d28da7b1fc98c1957050ac41d578eb6
mohanGups <74505938+mohanGups@users.noreply.github.com>
2023-10-31 19:42:46 +0100

    Extend basic conditionals with more snippets

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -1,31 +1,30 @@
+"""Conditionals"""
+
 # If statements are used to test for particular conditions and
 # respond appropriately.
+
+# ---------------------    Fundamentals   ---------------------
 
 # Conditional tests
 x = 1
 
 # equals
-x == 42
+print(x == 42)
 
 # not equal
-x != 42
+print(x != 42)
 
 # greater than or equal to
-x > 42
-x >= 42
+print(x > 42)
+print(x >= 42)
 
 # less than or equal to
-x < 42
-x <= 42
-
-# Conditional test with lists
-bikes = ['trek', 'specialized']
-'trek' in bikes
-'surly' not in bikes
+print(x < 42)
+print(x <= 42)
 
 # Assigning boolean values
 game_active = True
-can_edit = False
+CAN_VOTE = True
 
 # A simple if test
 age = 30
@@ -39,3 +38,35 @@ elif age < 18:
     ticket_price = 10
 else:
     ticket_price = 15
+
+# and/or
+if age >= 18 or age <= 65:
+    print("Can work")
+
+if age >= 18 and CAN_VOTE:
+    print("Can vote")
+
+# Simplified chain
+if 18 <= age <= 65:
+    print("Can work")
+
+# ---------------------    With Lists   ---------------------
+
+# Conditional test with lists
+bikes = ['trek', 'specialized']
+print('trek' in bikes)
+print('surly' not in bikes)
+
+# Testing if a value is not in a list
+banned_users = ['ann', 'chad', 'dee']
+user = 'erin'
+if user not in banned_users:
+    print("You can play!")
+
+# Checking if a list is empty
+players = []
+if players:
+    for player in players:
+        print("Player: " + player.title())
+else:
+    print("We have no players yet!")
